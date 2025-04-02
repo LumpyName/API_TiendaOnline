d5e837117b664e1ecb57ba19d70e2f5a8859d196
LumpyName <engerbenjamin8@gmail.com>
2025-04-02 10:29:37 -0400

    Es encerio como no me di cuenta que ise dos importanciones de 'models' (si es copy pages) pero que flojera recordar tantos nombres de tipos de tablas, lose doy pena

--- crud_basico/models.py
+++ crud_basico/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Usuario(models.Model):
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
